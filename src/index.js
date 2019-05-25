2e459b18300da1e2a7970023490d45e2e79fefde
alipianu <alexlipianu@live.ca>
2019-05-25 18:56:03 -0400

    added intersection observer polyfill

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import 'intersection-observer';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
