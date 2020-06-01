79dab8163b5ee6ce06165cf34def4f01f55bdacc
alipianu <alexlipianu@live.ca>
2020-05-31 22:53:47 -0400

    fix course link mismatch issue

--- src/containers/courses/courses.js
+++ src/containers/courses/courses.js
@@ -4,12 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Ribbon, OverflowScroll } from '../../components/components';
 
 const CoursesContainer = (props) => {
-  const half = Math.ceil(props.data.courses.length / 2);
+  const mid = Math.ceil(props.data.courses.length / 2);
   return (
     <OverflowScroll>
       <div className="container-courses animate__fade-in-down">
         <div className="container-courses__column">
-          {[...Array(half).keys()].map((i) => (
+          {[...Array(mid).keys()].map((i) => (
             <Ribbon key={i} type="right" title={props.data.courses[i].subject + ' ' + props.data.courses[i].code}>
               {/* prevent whitespace wrapping of (i) icon at end of course name by grouping desired words into 'blocks' with white-space: no-wrap */}
               {props.data.courses[i].title.split(' ').map((word, i, arr) => (
@@ -19,11 +19,11 @@ const CoursesContainer = (props) => {
           ))}
         </div>
         <div className="container-courses__column">
-          {[...Array(props.data.courses.length - half).keys()].map((i) => (
-            <Ribbon key={i} type="right" title={props.data.courses[half + i].subject + ' ' + props.data.courses[half + i].code}>
+          {[...Array(props.data.courses.length - mid).keys()].map((i) => (
+            <Ribbon key={i} type="right" title={props.data.courses[mid + i].subject + ' ' + props.data.courses[mid + i].code}>
               {/* prevent whitespace wrapping of (i) icon at end of course name by grouping desired words into 'blocks' with white-space: no-wrap */}
-              {props.data.courses[half + i].title.split(' ').map((word, i, arr) => (
-                <span key={i} className="format__textblock">{word}{i === arr.length - 1 && <a className="themify--active-color-hover" href={props.data.courses[i].url} rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={['fas', 'info-circle']} /></a>}</span>
+              {props.data.courses[mid + i].title.split(' ').map((word, i, arr) => (
+                <span key={i} className="format__textblock">{word}{i === arr.length - 1 && <a className="themify--active-color-hover" href={props.data.courses[mid + i].url} rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={['fas', 'info-circle']} /></a>}</span>
               ))}
             </Ribbon>
           ))}
--- src/containers/interests/interests.js
+++ src/containers/interests/interests.js
@@ -4,12 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { OverflowScroll } from '../../components/components';
 
 const InterestsContainer = (props) => {
-  const half = Math.floor(props.data.interests.length / 2);
+  const mid = Math.floor(props.data.interests.length / 2);
   return (
     <OverflowScroll>
       <div className="container-interests animate__fade-in-down">
         <div className="container-interests__column">
-          {[...Array(half).keys()].map((i) => (
+          {[...Array(mid).keys()].map((i) => (
             <p key={i}>
               <FontAwesomeIcon className="container-interests__icon" icon={[props.data.interests[i].icon.style || 'fas', props.data.interests[i].icon.name]} />
               {props.data.interests[i].text}
@@ -17,10 +17,10 @@ const InterestsContainer = (props) => {
           ))}
         </div>
         <div className="container-interests__column">
-          {[...Array(props.data.interests.length - half).keys()].map((i) => (
+          {[...Array(props.data.interests.length - mid).keys()].map((i) => (
             <p key={i}>
-              <FontAwesomeIcon className="container-interests__icon" icon={[props.data.interests[half + i].icon.style || 'fas', props.data.interests[half + i].icon.name]} />
-              {props.data.interests[half + i].text}
+              <FontAwesomeIcon className="container-interests__icon" icon={[props.data.interests[mid + i].icon.style || 'fas', props.data.interests[mid + i].icon.name]} />
+              {props.data.interests[mid + i].text}
             </p>
           ))}
         </div>
