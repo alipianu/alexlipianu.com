4ed8f9750be6da46caad4629c3c868ce00aca28f
alipianu <alexlipianu@live.ca>
2020-05-31 23:06:24 -0400

    fix course link mismatch issue

--- src/containers/courses/courses.js
+++ src/containers/courses/courses.js
@@ -12,8 +12,8 @@ const CoursesContainer = (props) => {
           {[...Array(mid).keys()].map((i) => (
             <Ribbon key={i} type="right" title={props.data.courses[i].subject + ' ' + props.data.courses[i].code}>
               {/* prevent whitespace wrapping of (i) icon at end of course name by grouping desired words into 'blocks' with white-space: no-wrap */}
-              {props.data.courses[i].title.split(' ').map((word, i, arr) => (
-                <span key={i} className="format__textblock">{word}{i === arr.length - 1 && <a className="themify--active-color-hover" href={props.data.courses[i].url} rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={['fas', 'info-circle']} /></a>}</span>
+              {props.data.courses[i].title.split(' ').map((word, j, arr) => (
+                <span key={j} className="format__textblock">{word}{j === arr.length - 1 && <a className="themify--active-color-hover" href={props.data.courses[i].url} rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={['fas', 'info-circle']} /></a>}</span>
               ))}
             </Ribbon>
           ))}
@@ -22,8 +22,8 @@ const CoursesContainer = (props) => {
           {[...Array(props.data.courses.length - mid).keys()].map((i) => (
             <Ribbon key={i} type="right" title={props.data.courses[mid + i].subject + ' ' + props.data.courses[mid + i].code}>
               {/* prevent whitespace wrapping of (i) icon at end of course name by grouping desired words into 'blocks' with white-space: no-wrap */}
-              {props.data.courses[mid + i].title.split(' ').map((word, i, arr) => (
-                <span key={i} className="format__textblock">{word}{i === arr.length - 1 && <a className="themify--active-color-hover" href={props.data.courses[mid + i].url} rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={['fas', 'info-circle']} /></a>}</span>
+              {props.data.courses[mid + i].title.split(' ').map((word, j, arr) => (
+                <span key={j} className="format__textblock">{word}{j === arr.length - 1 && <a className="themify--active-color-hover" href={props.data.courses[mid + i].url} rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={['fas', 'info-circle']} /></a>}</span>
               ))}
             </Ribbon>
           ))}
